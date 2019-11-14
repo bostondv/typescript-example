21ed3b79848a604bda0ddeeee1b71ebfb969eace
Boston Dell-Vandenberg <bostondv@gmail.com>
2019-11-14 09:19:54 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Button from "./Button";
+
+type AppProps = {};
+
+const App: React.FunctionComponent<AppProps> = () => {
+  return (
+    <div>
+      <Button>Hello, World!</Button>
+    </div>
+  );
+};
+
+export default App;
